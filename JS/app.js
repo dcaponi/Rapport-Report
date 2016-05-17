f41dbd00749358b934f144dca4a9930eeee6731e
dcaponi <d.caponi1@gmail.com>
2016-05-17 10:13:04 -0700

    initial commit: Layout Design

---
+++ JS/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+//Make the logo and header disappear when user scrolls down
+    $(window).scroll(function() {
+        if($(window).width() > 585){
+            if ($(document).scrollTop() > 50) {
+                $("header").fadeOut("slow");
+            } else {
+                $("header").fadeIn("slow");
+            }
+        }
+    });
+    
+//Button Scroll Effects
+    $('#about').on("click", function(event){
+        event.preventDefault();
+        $('html, body').animate({
+			scrollTop: ($('#aboutSection').offset().top)
+		},1000);
+    });
+
+    $('#goTo').on("click", function(event){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: ($('#searchSection').offset().top)
+        }, 1000);
+    });
+    
+    $('#searchButton').on("click", function(event){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: ($('#appSection').offset().top)
+        }, 1000);
+    });
+});
